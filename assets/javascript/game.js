431c9c491232bfaffcd515adddd6feb8edcdf85b
Ironam1 <ironam1@icloud.com>
2019-05-17 18:59:47 -0400

    game works, initially. some bugs to be fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,18 +18,86 @@ function startGame () {
     crystal2 = Math.floor((Math.random() * 3) + 3);
     crystal3 = Math.floor((Math.random() * 3) + 1);
     crystal4 = Math.floor((Math.random() * 3) + 7);
-    console.log(crystal1); 
+    console.log(crystal1);
+    userTotal = 0;
+    setDisplay();
+    playGame(); 
 }
 
-//function for display image will show crystals
+    
+
+//function setDisplay to reset target number and user total
 function setDisplay() {
     $("#comp-score").html(targetNumber);
     $("#user-score").html(userTotal);
-
+    console.log("middle " + userTotal);
 }
-    //this will assign the values to each crystal
-    //this will add consecutively the total for each click on the crystal
 
+function win () {
+    alert("YOU WIN!");
+    wins++;
+    $("#wins").text(wins);
+    startGame();
+    setDisplay();
+}   
+    
+function loss () {
+    alert("YOU LOSE!");
+    losses++;
+    $("#losses").text(losses);
+    startGame();
+    setDisplay();
+}
+console.log("next " + crystal1)
+    //function playGame each click on cyrstal will add consecutively
+function playGame () {
+    $("#one").on ('click', function (){
+        userTotal = userTotal + crystal1;
+        console.log("new " + userTotal);
+        $("#user-score").text(userTotal);
+            if (userTotal === targetNumber) {
+                win();
+            } else if (userTotal > targetNumber) {
+                loss();
+        }
+    })
+    $("#two").on ('click', function (){
+        userTotal = userTotal + crystal2;
+        console.log("new " + userTotal);
+        $("#user-score").text(userTotal);
+            if (userTotal === targetNumber) {
+                win();
+            } else if (userTotal > targetNumber) {
+                loss();
+        }
+    })
+    $("#three").on ('click', function (){
+        userTotal = userTotal + crystal3;
+        console.log("new " + userTotal);
+        $("#user-score").text(userTotal);
+            if (userTotal === targetNumber) {
+                win();
+            } else if (userTotal > targetNumber) {
+                loss();
+        }
+    })
+    $("#four").on ('click', function (){
+        userTotal = userTotal + crystal4;
+        console.log("new " + userTotal);
+        $("#user-score").text(userTotal);
+            if (userTotal === targetNumber) {
+                win();
+            } else if (userTotal > targetNumber) {
+                loss();
+        }
+    })
+}
+
+    //this will assign the values to each crystal
+    
+    //this will add consecutively the total for each click on the crystal
+//add function?
+    
 
 //function to compare user total to target number
     //if user total = target number user wins, if user total is more than 
